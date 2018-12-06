bcf36c223a13bf8a710778a2774f8c050a6dce46
Colin Witt <realbuddyc56@gmail.com>
2018-12-06 17:02:19 -0500

    Added Shipping methods and constructors
    also included bonus label() method to get the shipping half of the package label.

--- ShippingAddress.java
+++ ShippingAddress.java
@@ -2,5 +2,72 @@
  * <h1>Shipping Address</h1> Represents a shipping address
  */
 public class ShippingAddress {
-	//TODO
+    String name; //e.g. (Lawson Computer Science Building)
+    String address; //e.g. Street Address (305 N University St)
+    String city; // e.g. (West Lafayette)
+    String state; // e.g. IN
+    int zipCode; //e.g. 47907
+
+    public ShippingAddress() {
+        name = "";
+        address = "";
+        city = "";
+        state = "";
+        zipCode = 0;
+    }
+
+    public ShippingAddress(String name, String address, String city, String state, int zipCode) {
+        this.name = name;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setCity(String city){
+        this.city = city;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String label() {
+        String ret = "TO: " + name + "\n";
+        ret += address + "\n";
+        ret += city + ", " + state + ", " + zipCode;
+        return ret;
+    }
 }
